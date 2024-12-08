0ed7c59e620b77eb12da5432f9634cba13f4357b
abhishekpythons <abhishekpythons@gmail.com>
2024-12-09 05:09:05 +0530

    first commit

---
+++ djvue-vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome to DjVue 
+    </h1>
+    <p>A <strong>Django + Vue</strong> framework to create <strong>Déjà vu</strong>. <br>
+      I saw this site somewhere... 
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
